abc723312c3e97949db199fdab020ee472121db3
NamVu <namvt@live.com>
2016-05-30 16:51:36 +0700

    Assignment 3

---
+++ Assignment_3/rest-server/models/dishes.js
@@ -0,0 +1,68 @@
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// Will add the Currency type to the Mongoose Schema types
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+
+// create a schema
+// commentSchema
+var commentSchema = new Schema({
+	rating: {
+		type: Number,
+		min: 1,
+		max: 5,
+		required: true
+	},
+	comment: {
+		type: String,
+		required: true
+	},
+	author: {
+		type: String,
+		required: true
+	}
+}, {
+    timestamps: true
+});
+// dishSchema
+var dishSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String
+    },
+	category: {
+		type: String,
+		required: true
+	},
+	price: { 
+		type: Currency,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	label: {
+		type: String,
+		default: ''
+	},
+	image: {
+		type: String,
+		required: true
+	},
+	comments: [commentSchema]
+}, {
+    timestamps: true
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var dishes = mongoose.model('dishes', dishSchema);
+
+// make this available to our Node applications
+module.exports = dishes;
